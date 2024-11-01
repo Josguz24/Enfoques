dac53625faa4dc728208cda49e55e6d8599ada33
Jalberto <136873132+Josguz24@users.noreply.github.com>
2024-10-31 21:10:54 -0600

    Update 004_BusquedaProfundidadIterativa.py

--- Enfoque 1/004_BusquedaProfundidadIterativa.py
+++ Enfoque 1/004_BusquedaProfundidadIterativa.py
@@ -28,5 +28,4 @@ def busqueda_en_profundidad_iterativa(grafo, inicio, profundidad_maxima):
         busqueda_en_profundidad_limitada(grafo, inicio, limite)
 
 # Ejemplo de uso con profundidad máxima 3
-
 busqueda_en_profundidad_iterativa(grafo, 'A', 3)
