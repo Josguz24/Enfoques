47f360e63470e3ba20dd94c1579b59fd4c68d84c
Jalberto <136873132+Josguz24@users.noreply.github.com>
2024-10-31 21:12:57 -0600

    Update 036_BusquedaDeLaPolitica.py

--- Enfoque 1/036_BusquedaDeLaPolitica.py
+++ Enfoque 1/036_BusquedaDeLaPolitica.py
@@ -42,5 +42,6 @@ def busqueda_de_la_politica():
         mejorar_politica()
     return politica
 # Ejemplo de uso
+
 politica_optima = busqueda_de_la_politica()
 print("Política óptima encontrada:", politica_optima)
